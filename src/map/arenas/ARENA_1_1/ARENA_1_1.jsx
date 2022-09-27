2339352520802b29aa2da68684497654b1e2a146
Rancor38 <zak.rancor@hotmail.com>
2022-09-26 20:36:36 -0700

    Grid

--- src/map/arenas/ARENA_1_1/ARENA_1_1.jsx
+++ src/map/arenas/ARENA_1_1/ARENA_1_1.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import './Arena_1_1.css';
+import './ARENA_1_1.css';
 
-const Arena_1_1 = () => {
+const ARENA_1_1 = () => {
     return (
         <div class="parent">
-            
-        </div>
+            <div class="tile1"> </div>
+            <div class="tile2"> </div>
+            <div class="tile3"> </div>
+            <div class="tile4"> </div>
+            <div class="tile5"> </div>
+            <div class="tile6"> </div>
+            <div class="tile7"> </div>
+            <div class="tile8"> </div>
+            <div class="tile9"> </div>
+            <div class="tile10"> </div>
+            <div class="tile11"> </div>
+            <div class="tile12"> </div>
+            <div class="tile13"> </div>
+            <div class="tile14"> </div>
+            <div class="tile15"> </div>
+            <div class="tile16"> </div>
+            <div class="tile17"> </div>
+            <div class="tile18"> </div>
+            <div class="tile19"> </div>
+            <div class="tile20"> </div>
+            <div class="tile21"> </div>
+            <div class="tile22"> </div>
+            <div class="tile23"> </div>
+            <div class="tile24"> </div>
+            <div class="tile25"> </div>
+            <div class="tile26"> </div>
+            <div class="tile27"> </div>
+            <div class="tile28"> </div>
+            <div class="tile29"> </div>
+            <div class="tile30"> </div>
+</div>
     );
 };
 
-export default Arena_1_1;
+export default ARENA_1_1;
