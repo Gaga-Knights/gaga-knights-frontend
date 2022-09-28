6d9548728ab595d4f17cc20b8fa8d63f04e01a02
Rancor38 <zak.rancor@hotmail.com>
2022-09-27 20:06:42 -0700

    Big dragon cannot be run through

--- src/monsters/MONSTER_1_1/DRAGON.jsx
+++ src/monsters/MONSTER_1_1/DRAGON.jsx
@@ -5,7 +5,7 @@ import Texture from './dragon.gif'
 const DRAGON = () => {
     return (
         <div className='monster-position-arena'>
-            <div id='monster-hitbox' className='monster-position-f3'>
+            <div id='monster-hitbox' className='monster-position-b2 monster-position-b3 monster-position-b4 monster-position-c2 monster-position-c3 monster-position-c4 monster-position-d2 monster-position-d3 monster-position-d4'>
                 <img className='dragon' src={Texture} alt="dragon" />
             </div>
         </div>
